eb32826af72aae264771cda2a2921b2faf042c3b
Nathan Strong <nathanstrong.fx@gmail.com>
2020-08-20 15:05:59 -0700

    some changes

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -1,6 +1,10 @@
+
+
+
 import {Request, Response} from 'express';
-import {LESSONS} from './db-data';
-import {setTimeout} from 'timers';
+import {LESSONS} from "./db-data";
+import {setTimeout} from "timers";
+
 
 
 export function searchLessons(req: Request, res: Response) {
@@ -8,18 +12,18 @@ export function searchLessons(req: Request, res: Response) {
     const queryParams = req.query;
 
     const courseId = queryParams.courseId,
-        filter = String(queryParams.filter) || '',
-        sortOrder = queryParams.sortOrder,
-        pageNumber = parseInt(String(queryParams.pageNumber), 10) || 0,
-        pageSize = parseInt(String(queryParams.pageSize), 10);
+          filter = queryParams.filter || '',
+          sortOrder = queryParams.sortOrder,
+          pageNumber = parseInt(queryParams.pageNumber) || 0,
+          pageSize = parseInt(queryParams.pageSize);
 
-    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId === Number(courseId)).sort((l1, l2) => l1.id - l2.id);
+    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
 
     if (filter) {
-        lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
     }
 
-    if (sortOrder === 'desc') {
+    if (sortOrder == "desc") {
         lessons = lessons.reverse();
     }
 
@@ -29,7 +33,7 @@ export function searchLessons(req: Request, res: Response) {
 
     setTimeout(() => {
         res.status(200).json({payload: lessonsPage});
-    }, 1000);
+    },1000);
 
 
 }
