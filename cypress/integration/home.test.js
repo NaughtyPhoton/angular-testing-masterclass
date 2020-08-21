1935c224c387e87fd94ea1c19092febe49ada30f
Nathan Strong <nathanstrong.fx@gmail.com>
2020-08-21 10:07:51 -0700

    some changes

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,13 +1,27 @@
 describe('Home Page', () => {
-  it('displays a list of courses', () => {
+  beforeEach(() => {
     cy.server();
-    cy.fixture('courses.json').as('coursesJSON');
-    cy.route('/api/courses', '@coursesJSON').as('courses');
+    cy.fixture('courses.json')
+      .as('coursesJSON');
+    cy.route('/api/courses', '@coursesJSON')
+      .as('courses');
 
     cy.visit('/');
     cy.contains('All Courses');
+  });
 
+  it('displays a list of courses', () => {
     cy.wait('@courses');
     cy.get('mat-card').should('have.length', 9);
+  });
+
+  it('displays the advanced courses', () => {
+    cy.get('.mat-tab-label').should('have.length', 2);
+    cy.get('.mat-tab-label')
+      .last()
+      .click();
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .its('length')
+      .should('be.gt', 1);
   });
 });
